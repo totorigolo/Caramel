41c1945a0cb0183ccf23810e6f0a68590b603cb1
ElisePetit <petit.elise06@gmail.com>
2018-03-13 10:55:48 +0100

    new tests

---
+++ tests/grammar/valid/v5_complete_program.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+/*
+ * The is a multi line
+ *
+ * comment
+ */
+
+int main(int, char) {
+    int32_t i;
+    int64_t j;
+    ;
+    char a;
+    ;
+    ;
+// Comment here
+    int32_t d, e;// Other comment here
+
+    char c = 'c';
+    int32_t k = 1;
+    int32_t l = -25;
+    int64_t m = 10;
+
+    int32_t espaceChar = '\n';
+    int32_t espaceChar = '\n';
+    int32_t espaceChar = '\r';
+    int32_t espaceChar = '\t';
+    int32_t espaceChar = '\\';
+    int32_t espaceChar = '\'';
+    int32_t espaceChar = '\"';
+
+    int32_t line
+            = 4 + 4 - (5 * 6) ;
+
+    int32_t p = (
+      1 + 2
+    );
+
+    void f() {}
+
+    int32_t func(int a, int) {}
+
+    puts('c');
+
+    int32_t add = 3 + 2 - 1;
+
+    int32_t mult = 1 * 2 / 3 % 5;
+
+    while(i < 10) {
+        if (i % 2 == 0) {
+            put('a' + i);
+        }
+        else if (0 != 1 || 0 < 1 &&  0 <= 1) {
+          m--;
+        }
+        else {
+          i = i+2;
+        }
+        i++;
+    }
+    return 0;
+}
