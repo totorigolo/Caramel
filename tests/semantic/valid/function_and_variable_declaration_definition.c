126963d3dae38267b2a93e2ad6d91d983e531753
Thomas Lacroix <toto.rigolo@free.fr>
2018-03-22 09:40:02 +0100

    New tests folder hierarchy for semantic testing / validation.

---
+++ tests/semantic/valid/function_and_variable_declaration_definition.c
@@ -0,0 +1,18 @@
+/*
+ * Multi-line comment.
+ */
+
+#include <stdio.h>
+
+void foo(int8_t a0, int8_t b0, int8_t c0);
+
+int32_t main(int32_t, char c) {
+    int32_t a, b = 0;
+    char c = '!';
+    return 0;
+}
+
+void foo(int8_t a1, int8_t b1, int8_t c1)
+{
+    char a0, a1;
+}
