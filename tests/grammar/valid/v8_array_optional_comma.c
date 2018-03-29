99739c26c3973d3c20f1d7d9385a84b877345f5c
Thomas Lacroix <toto.rigolo@free.fr>
2018-03-29 18:56:36 +0200

    Fix wrong test.

--- tests/grammar/valid/v8_array_optional_comma.c
+++ tests/grammar/valid/v8_array_optional_comma.c
@@ -1,3 +1,24 @@
+/*
+ * Multi-line comment.
+ */
 
-typedef int64_t money_t;
-typedef int32_t age_t;
+#include <stdio.h>
+#include <stdint.h>
+
+
+void foo(
+        int16_t a,
+        int32_t b,
+) {
+    return 0;
+}
+
+int32_t main(int32_t, char argv[]) {
+    int32_t tab[] = {
+            1,
+            2,
+            3,
+            //3,
+    };
+    return 0;
+}
