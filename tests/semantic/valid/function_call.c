39044df67e8de37408cbef5f7de4c8627025f2ec
Thomas Lacroix <toto.rigolo@free.fr>
2018-03-31 22:45:27 +0200

    Minor fix for the functionCall.c test.

--- tests/semantic/valid/function_call.c
+++ tests/semantic/valid/function_call.c
@@ -6,12 +6,12 @@ int32_t foo(int16_t a, int32_t b, int64_t c);
 int32_t foo(int16_t a, int32_t b, int64_t c);
 int32_t foo(int16_t a, int32_t b, int64_t c);
 
-//int32_t main(int32_t, char argv) {
-//    int16_t var = 5;
-//    foo(var, 2, 3);
-//    foo(2, var, 4);
-//    return 0;
-//}
+int32_t main(int32_t, char argv) {
+    int16_t var = 5;
+    foo(var, 2, 3);
+    foo(2, var, 4);
+    return 0;
+}
 
 int32_t foo(int16_t a, int32_t b, int64_t c) {
     return b;
