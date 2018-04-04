118cb78b3321f6d2f665e25183592a0f1990ac05
Aurore LOISEAU <aurore.loiseau@insa-lyon.fr>
2018-04-05 01:05:12 +0200

    Fix returnName of FunctionCallInstruction.

--- tests/back-end/if_block_complexe_cond.c
+++ tests/back-end/if_block_complexe_cond.c
@@ -4,9 +4,14 @@
 
 #include <stdint.h>
 
+int32_t sum(int32_t a, int32_t b) {
+    return a + b;
+}
+
 int32_t main() {
     int32_t a = 0;
-    if (a + 1) {
+    putchar('0' + sum(2, 3));
+    if (sum(2, 3) - 5) { // 5 - 5 = 0
         putchar('a');
         putchar('\n');
     }
