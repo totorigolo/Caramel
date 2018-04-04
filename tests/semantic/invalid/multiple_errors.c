3f9ff1e292d24e9aa2b5a94505eb1798406d1d21
ElisePetit <petit.elise06@gmail.com>
2018-04-04 15:37:56 +0200

    manage multiple errors

---
+++ tests/semantic/invalid/multiple_errors.c
@@ -0,0 +1,21 @@
+/*
+ * Multi-line comment.
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int32_t foo(int32_t a, int32_t b, int32_t c);
+
+int32_t main() {
+    int32_t a, b = 0;
+    int32_t c = d;
+    return 0;
+}
+
+int32_t foo(int32_t a, int32_t b)
+{
+    int32_t a2 = 'h', b2;
+    return 0;
+}
+
