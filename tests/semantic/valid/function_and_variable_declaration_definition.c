cc5a2f9d23365b1e4c1fa061d94930e2514bec27
Loic R <rouquette.loic@gmail.com>
2018-04-04 10:34:53 +0200

    Reestablish test to his original meaning

--- tests/semantic/valid/function_and_variable_declaration_definition.c
+++ tests/semantic/valid/function_and_variable_declaration_definition.c
@@ -10,15 +10,11 @@ int32_t foo(int32_t a, int32_t b, int32_t c);
 int32_t main() {
     int32_t a, b = 0;
     int32_t c = '!';
-    foo('\n', 'a', 'd');
-    putchar('d');
     return 0;
 }
 
 int32_t foo(int32_t a, int32_t b, int32_t c)
 {
     int32_t a2 = 'h', b2, c2;
-    putchar(c);
-    putchar(a2);
     return 0;
 }
